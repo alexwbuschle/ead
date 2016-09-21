ce39406d5ad593aa1c108cd8b4cdba96b82148c8
Alex Buschle <alexwbuschle@gmail.com>
2016-09-21 07:30:44 -0300

    Specify version

--- elasticsearch-mysite/recipes/default.rb
+++ elasticsearch-mysite/recipes/default.rb
@@ -10,6 +10,8 @@ include_recipe 'chef-sugar'
 elasticsearch_user 'elasticsearch'
 elasticsearch_install 'elasticsearch' do
   type node['elasticsearch']['install_type'].to_sym # since TK can't symbol.
+  version "2.2.0"
+  action :install
 end
 elasticsearch_configure 'elasticsearch'
 elasticsearch_service 'elasticsearch'
